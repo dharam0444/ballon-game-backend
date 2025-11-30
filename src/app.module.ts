18adc11e1585c0f3cc42a5772ba5d52945aa774d
Dharmendra Shivhare <dharmendra.shivhare@hiddenbrains.in>
2025-11-30 23:18:41 +0530

    db port related changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,16 @@ import { Player } from './players/entities/player.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL + '?sslmode=require',
-      ssl: { rejectUnauthorized: false },
-      synchronize: process.env.NODE_ENV !== 'production',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+      ssl: {
+        rejectUnauthorized: false, // important for self-signed cert
+      },
       entities: [Player],
     }),
     PlayersModule,
